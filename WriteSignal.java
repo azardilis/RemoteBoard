ac45e83c5925b89744265d9f20f754b672d53bfa
azardilis <azardilis@gmail.com>
2012-04-28 13:29:31 +0100

    new representation of the signal sent to subscribers

---
+++ WriteSignal.java
@@ -0,0 +1,18 @@
+import java.awt.Point;
+import java.io.Serializable;
+
+public class WriteSignal implements Serializable {
+
+    public static final int MOUSE_RELOC = 0;
+    public static final int MOUSE_WRIT = 1;
+    private Point point;
+    private int signal;
+
+    public WriteSignal(int sig, Point p) {
+	this.point = p;
+	this.signal = sig;
+    }
+
+    public int getSignal(){return this.signal;}
+    public Point getPoint(){return this.point;}
+}
